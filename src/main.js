262c671084cfb4b1df52e519c68216000b312063
jaxonRuiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-02-14 22:55:03 -0800

    comment

--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
 // Game title: Surviving
 // Author: jaxon ruiz
-// ~8 hours worked
-// (still using semicolons because of C trauma)
-// I did not sufficiently preplan the runner, and changed my mind FAR too much.
-// by the end my code is a mess of various attempted ideas, many of which are 
-// suboptimal or incomplete.
-// i feel like i have (er) "creative" coding solutions to some things. to say the least...
-// also im working on this on two different setups, and framerates are heavily affecting gameplay feel...
+// ~25 hours worked
+
+// technically interesting (lol)
+    /* i definitely went into this project with too many broad technical ideas for mechanics and stuff, 
+    and ended up not really being able to focus on any single idea for most of the time i spent working, 
+    which is now left in various deleted blocks of code. i ended up settling on more of a physics based runner
+    and had to play around a lot with the physics engine to tweak the amounts of impact getting hit by an obstacal 
+    would cause the player. 
+    im also somewhat proud of how i ended up doing the obstacle spawning/managing with the seperate MapGenerator class,
+    which would handle the creation and storage of the obstacles, though i feel like the code is a bit of a mess 
+    */
+
+// visual style
+    /* i wanted to make a flash style game with this project, but wasn't really able to spend as much time working on 
+    the visuals as i wouldve wanted to, however i'm somewhat proud of the multi layered parallax in the background to give
+    the impression of high speeds. i think the depth created by several layers of parallax helps really sell the speedy effect
+
+
+// misc notes
+/* I originally wanted to make a game based on nostalgia for old flash games but ended up drifting away 
+too much from my initial idea, so its kind of ended up as a mishmash of various thematics. However i think 
+its close enough to the spirit of some of the flash games i played
+(still using semicolons because of C trauma)
+I did not sufficiently preplan the runner, and changed my mind FAR too much.
+by the end my code is a mess of various attempted ideas, many of which are 
+suboptimal or incomplete.
+i feel like i have (er) "creative" coding solutions to some things. to say the least...
+also im working on this on two different setups, and framerates are heavily affecting gameplay feel...
+*/
 
 let config = {
     type: Phaser.AUTO,
